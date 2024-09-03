bb32450126afb77a11adb10a9bb76913c675bd14
FrostVin108 <vincentsidarta.hu@gmail.com>
2024-09-03 14:41:52 +0700

    last

--- app/Models/sparepart.php
+++ app/Models/sparepart.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\hasOne;
+use Laravel\Scout\Searchable;
 
 
 class sparepart extends Model
 {
+    use Searchable;
     protected $table = 'pc-specification';
     protected $primaryKey = 'id';
 
